101f4a019ccb5c5d21a4af25f4ec9a45d3d38374
nicoxx <nicoxx@hotmail.fr>
2015-06-09 22:27:15 +0200

    initial commit

---
+++ src/func_tests/MVP_tests.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+import unittest
+
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_see_default_links_and_updates(self):
+        #Kevin connects to the site
+        self.browser.get('http://localhost:8080')
+
+        #He notices the title and the tab is called "gator"
+        self.assertEquals('Gator', self.browser.title)
+        #self.fail('TODO')
+
+        #He sees a list a list of link : the ones of the site hckrnews
+
+        #the links on hckrnews changes
+
+        #he refreshes the page
+
+        #he sees the page updated with the new links of hckrnews
+
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ src/server/gator.py
@@ -0,0 +1,33 @@
+# [START imports]
+import os
+import urllib
+
+from google.appengine.api import users
+from google.appengine.ext import ndb
+
+import jinja2
+import webapp2
+
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+# [END imports]
+
+
+# [START main_page]
+class MainPage(webapp2.RequestHandler):
+
+    def get(self):
+
+        template = JINJA_ENVIRONMENT.get_template('index.html')
+        self.response.write(template.render())
+# [END main_page]
+
+
+
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+], debug=True)
