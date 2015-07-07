d15f98ea2248c5cb9fe90d16877f85ea2f8e1af7
nicoxx <nicoxx@hotmail.fr>
2015-07-07 23:11:50 +0200

    make hadrcoded list of links with jinja template + move .html in template folder

--- src/functests/test_homepage.py
+++ src/functests/test_homepage.py
@@ -13,21 +13,27 @@ class NewVisitorTests(unittest.TestCase):
 
     def test_see_default_links(self):
 
-        #Kevin connects to the site
+        # Kevin connects to the site
         self.browser.get('http://localhost:8080')
 
-        #He notices the title and the tab is called "gator"
-        self.assertEquals('Gator', self.browser.title)
-        self.assertEquals('gator.life 2.0 !', self.browser.find_element_by_name('title').text)
-        #self.fail('TODO')
+        # The tab is called "Gator Life !"
+        self.assertEquals('Gator Life !', self.browser.title)
 
-        #He sees a list a list of link : the ones of the site hckrnews
+        # The title of the page is 'Gator.Life, the best of the web just for you !'
+        tab_title = self.browser.find_element_by_name('title').text
+        self.assertEquals('Gator.Life, the best of the web just for you !', tab_title)
 
-        #the links on hckrnews changes
+        # There is two links on the page : google.com, gator.life
+        links = self.browser.find_elements_by_name('link')
+        self.assertEqual(2, len(links))
+        self.assertEqual('google.com', links[0].text)
+        self.assertEqual('gator.life', links[1].text)
 
-        #he refreshes the page
+        # kevin clicks on google.com and wait to go there
+        google_link = links[0]
+        google_link.click()
+        self.assertEqual("Google", self.browser.title)
 
-        #he sees the page updated with the new links of hckrnews
 
 if __name__ == '__main__':
     unittest.main()
--- src/server/server/gator.py
+++ src/server/server/gator.py
@@ -1,29 +1,51 @@
-# [START imports]
 import os
-
 import webapp2
 import jinja2
 
-JINJA_ENVIRONMENT = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
-    extensions=['jinja2.ext.autoescape'],
-    autoescape=True)
-# [END imports]
+CGI_VARIABLE_SERVER_SOFTWARE = 'SERVER_SOFTWARE'
+
+def is_dev_env_server():
+    return os.environ.get(CGI_VARIABLE_SERVER_SOFTWARE, '').startswith('Dev')
+
+DEBUG_MODE = is_dev_env_server()
+
+class Link(object):
+    def __init__(self, link, text):
+        self.link = link
+        self.text = text
 
 
-# [START main_page]
-class MainPage(webapp2.RequestHandler):
+class MainPageHandler(webapp2.RequestHandler):
     def get(self):
-
         template = JINJA_ENVIRONMENT.get_template('index.html')
-        self.response.write(template.render())#pylint: disable=no-member
+
+
+        links = [
+            Link(link='https://www.google.com', text='google.com'),
+            Link(link='gator.life', text='gator.life')
+        ]
+
+        template_values = {
+            'links': links
+        }
+
+
+        self.response.write(template.render(template_values))#pylint: disable=no-member
         # bug in pylint with jinja templates:
         # https://bitbucket.org/logilab/pylint/issue/490/jinja-templates-are-handled-as-str
-# [END main_page]
+
+ROUTING = [
+    ('/', MainPageHandler),
+]
 
 
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__) + '/templates'),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
 
 # 'app' name is mandatory for webbapp
-app = webapp2.WSGIApplication([ #pylint: disable=invalid-name
-    ('/', MainPage),
-], debug=True)
+app = webapp2.WSGIApplication(ROUTING, debug=DEBUG_MODE) #pylint: disable=invalid-name
+
+
+
