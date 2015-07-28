c62b3c0d5fa6b342ed9e3df7dfb4d1bf14b42c59
nicoxx <nicoxx@hotmail.fr>
2015-07-28 23:41:42 +0200

    try to deploy with script in root folder

---
+++ deploy_to_gae.sh
@@ -0,0 +1,1 @@
+./lib/google_appengine/appcfg.py -A gator-01 --skip_sdk_update_check --oauth2_access_token=$GAE_TOKEN --oauth2_refresh_token=$GAE_REFRESH_TOKEN update ./src/server/server
--- scripts/deploy_to_gae.sh
+++
@@ -1,1 +0,0 @@
-../lib/google_appengine/appcfg.py -A gator-01 --skip_sdk_update_check --oauth2_access_token=$GAE_TOKEN --oauth2_refresh_token=$GAE_REFRESH_TOKEN update ../src/server/server
