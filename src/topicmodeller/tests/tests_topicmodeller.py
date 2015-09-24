c858114b33689da10a276a7933fa4193327f51d2
Mohamed Mokhtari <mohamed.mokhtari2@gmail.com>
2015-09-24 22:21:01 +0200

    Fix pylint errors

--- src/topicmodeller/tests/tests_topicmodeller.py
+++ src/topicmodeller/tests/tests_topicmodeller.py
@@ -1,43 +1,46 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from topicmodeller.topicmodeller import TopicModeller, _decode, _filter_latin_words, _readable_document, _remove_stop_words, \
-    _word_tokenize
+from topicmodeller.topicmodeller import TopicModeller, _decode, _filter_latin_words, _readable_document, \
+    _remove_stop_words, _word_tokenize
 
 import os
 import unittest
 
 
 class TopicModellerTests(unittest.TestCase):
-    tm_documents = [ ([], [ [u'BattleBots', u'producers', u'definitely', u'looking', u'forward', u'feeling',
-               u'confident', u'ABC', u'renew', u'series', u'second', u'season', u'seventh', u'count', u'five',
-               u'seasons', u'ran', u'Comedy', u'Central', u'And', u'\'ve', u'definitely',
-               u'given', u'thought', u'like', u'similarly', u'like', u'change'],
-              [ u'In', u'many', u'ways', u'an', u'open', u'ournament', u'helps', u'here', u'But', u'the',
-                u'producers', u'encourage', u'competitors', u'to', u'find', u'loopholes', u'in', u'the',
-                u'rules' ] ]) ]
+    tm_documents = [([], [[u'BattleBots', u'producers', u'definitely', u'looking', u'forward', u'feeling', u'confident',
+                           u'ABC', u'renew', u'series', u'second', u'season', u'seventh', u'count', u'five', u'seasons',
+                           u'ran', u'Comedy', u'Central', u'And', u'\'ve', u'definitely', u'given', u'thought', u'like',
+                           u'similarly', u'like', u'change'],
+                          [u'In', u'many', u'ways', u'an', u'open', u'ournament', u'helps', u'here', u'But', u'the',
+                           u'producers', u'encourage', u'competitors', u'to', u'find', u'loopholes', u'in', u'the',
+                           u'rules']])]
 
     def setUp(self):
-        self.scraper_document = _decode(open('scraper_documents/2015-08-01 18:00:22.926317_8.json').read())
-
-        for file in os.listdir('tm_data'):
-            os.remove(os.path.join('tm_data', file))
+        for tm_file in os.listdir('tm_data'):
+            os.remove(os.path.join('tm_data', tm_file))
 
     def test_readable_document(self):
-        readable_document = _readable_document(self.scraper_document.html_content)
+        directory = os.path.dirname(os.path.abspath(__file__))
+        html_content = _decode(open(os.path.join(directory, 'scraper_documents/2015-08-01 18:00:22.926317_8.json'))
+                               .read()).html_content
+        readable_document = _readable_document(html_content)
+
         self.assertTrue(u'just aired its thrilling, flame thrower-filled, metal shrapnel-rich'
                         in readable_document)
         self.assertTrue(u'1.) Continue to emphasize the competition as a sport.'
                         in readable_document)
 
     def test_word_tokenize(self):
-        document = u"\"BattleBots\" producers are definitely looking forward and feeling confident that ABC will renew the " \
-                   u"series for a second season (seventh if you count the five seasons it ran on Comedy Central from 2000 to " \
-                   u"2002). And they've definitely given some thought as to what they'd like to do similarly and what they'd " \
-                   u"like to change up."
+        document = u"\"BattleBots\" producers are definitely looking forward and feeling confident that ABC will renew " \
+                   u"the series for a second season (seventh if you count the five seasons it ran on Comedy Central from " \
+                   u"2000 to 2002). And they've definitely given some thought as to what they'd like to do similarly and " \
+                   u"what they'd like to change up."
         word_tokenized = _word_tokenize(document)
 
-        words = [u'will', u'renew', u'the', u'series', u'for', u'a', u'second', u'season', u'seventh', u'if', u'you', u'count']
+        words = [u'will', u'renew', u'the', u'series', u'for', u'a', u'second', u'season', u'seventh', u'if', u'you',
+                 u'count']
         self.assertTrue(all(x in word_tokenized for x in words))
 
     def test_remove_stop_words(self):
@@ -60,14 +63,14 @@ class TopicModellerTests(unittest.TestCase):
         self.assertTrue(not _filter_latin_words([u'1000']))
 
     def test_initialize_tm_dictionary(self):
-        tm = TopicModeller('tm_data', 2)
-        tm.initialize_dictionary(self.tm_documents)
+        topicmodeller = TopicModeller('tm_data', 2)
+        topicmodeller.initialize_dictionary(self.tm_documents)
 
         all_words = (word for (_, batched_document) in self.tm_documents
-                   for document in batched_document
-                   for word in document)
+                     for document in batched_document
+                     for word in document)
 
-        self.assertTrue(all(word in tm.dictionary.values() for word in all_words))
+        self.assertTrue(all(word in topicmodeller.dictionary.values() for word in all_words))
 
 
 if __name__ == '__main__':
