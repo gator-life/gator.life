b8e8ff8bb21eecf7d129dea3855f79c76c006dcb
Mohamed Mokhtari <mohamed.mokhtari2@gmail.com>
2015-11-08 19:41:13 +0100

    First version of a naive learner.

---
+++ src/learner/learn.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from learner.learner import learn
+
+import logging
+
+
+learn(documents_folder = '/home/mohamed/Development/Data/TopicModelling/json', min_grade=0.5)
