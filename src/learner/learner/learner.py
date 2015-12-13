617e706e3d5792fc34146c1fadd06924b2f3f2cc
nicoxx <nicoxx@hotmail.fr>
2015-12-13 23:02:58 +0100

    use cosine similarity and numpy/scipy to match users and doc

--- src/learner/learner/learner.py
+++ src/learner/learner/learner.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from scipy.spatial import distance
+import numpy as np
 
 
 class UserDocMatching(object):
@@ -9,8 +11,16 @@ class UserDocMatching(object):
 
 
 def compute_user_doc_matching(user_feature_vectors, document_feature_vector, min_grade):
-    users_doc_matching = []
-    for user_feature_vector in user_feature_vectors:
-        grade = sum(a * b for (a, b) in zip(user_feature_vector, document_feature_vector))
-        users_doc_matching.append(UserDocMatching(is_doc_relevant=grade > min_grade, grade=grade))
-    return users_doc_matching
+    grade_vec = similarity_by_row(user_feature_vectors, document_feature_vector)
+    return [UserDocMatching(is_doc_relevant=grade > min_grade, grade=grade) for grade in np.nditer(grade_vec)]
+
+
+def similarity_by_row(matrix, vector):
+    """
+    compute the cosine similarity (normalized dot product) for each row of 'matrix' against 'vector'
+    :param matrix: array of array or numpy matrix of dimension (n elements, p features)
+    :param vector: vector of dimension p features
+    :return: vector of size n
+    """
+    vec_as_1_p_matrix = [vector]
+    return 1 - distance.cdist(matrix, vec_as_1_p_matrix, metric='cosine')
--- src/learner/tests/test_learner.py
+++ src/learner/tests/test_learner.py
@@ -1,27 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+from math import sqrt
 import unittest
-
 from learner.learner import compute_user_doc_matching
+
+
+# test implementation of L2 norm without re-using numpy code
+def norm(vec):
+    return sqrt(sum(x*x for x in vec))
 
 
 class LearnerTests(unittest.TestCase):
 
     def test_compute_user_doc_matching_with_grade_above_should_flag_doc_as_relevant(self):
+        user1 = [0.49, 1.0, 0.0]
+        user2 = [0.5, 1.0, 0.1]
+        users = [user1, user2]
+        doc = [1.0, 0.5, 0.1]
 
-        user1_feature_vector = [0.49, 1.0, 0.0]
-        user2_feature_vector = [0.5, 1.0, 0.1]
-
-        user_feature_vectors = [user1_feature_vector, user2_feature_vector]
-
-        matching = compute_user_doc_matching(
-            user_feature_vectors=user_feature_vectors, document_feature_vector=[1.0, 0.5, 0.1], min_grade=1.0)
+        matching = compute_user_doc_matching(user_feature_vectors=users, document_feature_vector=doc, min_grade=0.8)
 
         self.assertFalse(matching[0].is_doc_relevant)
-        self.assertEqual(0.99, matching[0].grade)
+        self.assertEqual(0.99/(norm(user1)*norm(doc)), matching[0].grade)
         self.assertTrue(matching[1].is_doc_relevant)
-        self.assertEqual(1.01, matching[1].grade)
+        self.assertEqual(1.01/(norm(user2)*norm(doc)), matching[1].grade)
 
 
 if __name__ == '__main__':
