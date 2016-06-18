ae2b8f62a1957ebc811f406bc599aed831ff1149
nicoxx <nicoxx@hotmail.fr>
2016-06-18 23:05:46 +0200

    Add a centralized utcnow
    the python 2 framework datetime.utcnow gives a date without timezone info
    This makes comparing it to date from database fails (because datetimes loaded from gcloud datastore have timezones, that was not the case before)

---
+++ src/common/common/datehelper.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+import pytz
+
+
+def utcnow():
+    """
+    datetime.utcnow() returns by default a timezone unaware datetime (without timezone info),
+    but datetime returned by gcloud datastore a timezone aware, so a comparison between the two fails
+    :return: timezone aware version of datetime.utcnow()
+    """
+    return datetime.utcnow().replace(tzinfo=pytz.utc)
