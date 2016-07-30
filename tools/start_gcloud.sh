a711936c37ab47c96c2440818223f98527a84308
Nicolas Lecrique <nicoxx@hotmail.fr>
2016-07-30 19:37:50 +0200

    fix start_gcloud script suicide
    since I added shebang on script, 'pkill gcloud" command was killing the script itself
    because it contains gcloud. I make regex more precise to fix this

--- tools/start_gcloud.sh
+++ tools/start_gcloud.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # kill previous instance of gcloud and free port if needed
-pkill gcloud
+pkill ^gcloud$
 fuser -k 33001/tcp
 gcloud --quiet beta emulators datastore start --project gator-01 --no-store-on-disk --host-port localhost:33001 --consistency=1.0
