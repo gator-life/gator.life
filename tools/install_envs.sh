7fce834983485958b6f8c9290d8e30570c21849a
Nicolas Lecrique <nicoxx@hotmail.fr>
2016-08-21 10:32:50 +0200

    [python3] upgrade and adapt environment to python3

--- tools/install_envs.sh
+++ tools/install_envs.sh
@@ -5,17 +5,17 @@
 mkdir -p lib
 
 rm -rf lib/google-cloud-sdk
-wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-119.0.0-linux-x86_64.tar.gz -nv
-tar zxvf google-cloud-sdk-119.0.0-linux-x86_64.tar.gz -C lib
-rm google-cloud-sdk-119.0.0-linux-x86_64.tar.gz
+wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-122.0.0-linux-x86_64.tar.gz -nv
+tar zxvf google-cloud-sdk-122.0.0-linux-x86_64.tar.gz -C lib
+rm google-cloud-sdk-122.0.0-linux-x86_64.tar.gz
 
-pip install -r requirements.txt
-pip install -r test_requirements.txt
+pip3 install -r requirements.txt
+pip3 install -r test_requirements.txt
 # server unit tests are run in this "default" env so we need server requirements in it
-pip install -r src/server/server/requirements.txt
-pip install -e src/common -e src/scraper -e src/server -e src/topicmodeller -e src/learner -e src/orchestrator
-python -m nltk.downloader stopwords punkt
+pip3 install -r src/server/server/requirements.txt
+pip3 install -e src/common -e src/scraper -e src/server -e src/topicmodeller -e src/learner -e src/orchestrator
+python3 -m nltk.downloader stopwords punkt
 
 #appengine_env env is used to run flask server in same context as on appengine
-virtualenv appengine_env
+python3 -m venv appengine_env
 appengine_env/bin/pip install -r src/server/server/requirements.txt
