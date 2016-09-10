980d8ac61f7566cffd8e439b06ea7a7f01fe23dd
Mohamed Mokhtari <mohamed.mokhtari2@gmail.com>
2016-09-10 21:06:38 +0200

    Use the downloaded version of gcloud (it seems that there is another version installed on travis)

--- tools/gen_app_dockerfile.sh
+++ tools/gen_app_dockerfile.sh
@@ -7,7 +7,7 @@ rm -f src/server/Dockerfile src/server/.dockerignore
 
 # Generate Dockerfile and .dockerignore files
 # echo "N" : Allow to respond "No" to the command asking if we want to update app.yaml to set runtime to custom.
-echo "N" | gcloud beta app gen-config --config src/server/app.yaml --custom src/server
+echo "N" | lib/google-cloud-sdk/bin/gcloud beta app gen-config --config src/server/app.yaml --custom src/server
 
 echo "ENV DATASTORE_HOST="$DATASTORE_HOST >> src/server/Dockerfile
 echo "ENV TEST_ENV 'True'" >> src/server/Dockerfile
