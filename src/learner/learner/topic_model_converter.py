61d2b76f301a840c2d4a4ab8fe7a9a00223ff2aa
Nicolas Lecrique <nicoxx@hotmail.fr>
2016-10-20 22:20:20 +0200

    Add logic to convert an vector from a topic model to another
    Will be useful for migration (but still needs a high level script to update database)

---
+++ src/learner/learner/topic_model_converter.py
@@ -0,0 +1,71 @@
+from itertools import chain, izip
+import numpy as np
+
+
+class TopicModelConverter(object):
+    """
+    Service to convert a feature vector expressed in a origin topic model to the best approximation
+    of this vector in a target topic model.
+    """
+
+    def __init__(self, origin_model, target_model):
+        """
+        :param origin_model: list of topics of "old" model, each topic is a list of tuple (word, weight)
+        :param target_model: list of topics of "new" model, each topic is a list of tuple (word, weight)
+        """
+        words = set(word for topic in chain(origin_model, target_model) for (word, _) in topic)  # unique words
+        word_to_index = dict(izip(words, range(len(words))))
+        origin_model_basis = self._build_basis_matrix(word_to_index, origin_model)
+        target_model_basis = self._build_basis_matrix(word_to_index, target_model)
+        self._projector = _VectorSpaceProjector(origin_model_basis, target_model_basis)
+
+    @staticmethod
+    def _build_basis_matrix(word_to_index, model):
+        model_basis = np.zeros((len(word_to_index), len(model)))
+        for index_topic, topic in enumerate(model):
+            for word, weight in topic:
+                model_basis[word_to_index[word], index_topic] = weight
+        return model_basis
+
+    def compute_target_vector(self, origin_vector):
+        """
+        :param origin_vector: classification of element (doc or user profile) in origin_model,
+        list of double of size origin_model.nb_topics
+        :return: approximation of classification of this element (doc or user profile) in target_model
+        list of double of size target_model.nb_topics
+        """
+        np_origin_vector = np.asarray(origin_vector)
+        np_target_vector = self._projector.project_on_target_space(np_origin_vector)
+        target_vector = np_target_vector.tolist()
+        return target_vector
+
+
+class _VectorSpaceProjector(object):
+    """
+    Service to project a vector from a subspace to another inside a common vector space
+    """
+
+    def __init__(self, origin_space_basis, target_space_basis):
+        """
+        :param origin_space_basis: numpy matrix dim_global_space * dim_origin_subspace
+        :param target_space_basis: numpy matrix dim_global_space * dim_target_subspace
+        -let's call A and B the matrices where each column 'i' is the coordinates of i-th base vector of global subspace
+        coordinates, of respectively origin and target subspace.
+        -let's call 'a' a vector of coordinates expressed in origin_space referential.
+        -We want to find 'b', a vector of coordinates expressed in target_space that is the best approximation of 'a'
+        -Best is understood in the sens of the L2 natural norm of the global vector space
+        -We search 'b'= arg_min ||Aa-Bb||. By taking Grad[(Aa-Bb).(Aa-Bb)] = 0, we show that:
+        This is solved by the linear equation : ( B^T * B ) b = ( B^T * A ) a.
+        """
+        # pre-compute left hand side (B^T * B) and right hand side ( B^T * A )
+        target_space_transposed = target_space_basis.transpose()
+        self._left_hand_side = np.dot(target_space_transposed, target_space_basis)
+        self._right_hand_side = np.dot(target_space_transposed, origin_space_basis)
+
+    def project_on_target_space(self, vector_in_origin_basis):
+        """
+        :param vector_in_origin_basis: vector expressed in origin_space_basis
+        :return: best projection of input vector on target_space expressed in target_space_basis
+        """
+        # solve linear equation ( B^T * B ) b = ( B^T * A ) a
+        return np.linalg.solve(self._left_hand_side, np.dot(self._right_hand_side, vector_in_origin_basis))
---
+++ src/learner/tests/test_topic_model_converter.py
@@ -0,0 +1,104 @@
+import unittest
+import numpy as np
+from learner.topic_model_converter import TopicModelConverter
+
+
+class TopicModelConverterTests(unittest.TestCase):
+
+    def test_compute_target_vector_with_same_model_return_same_vector(self):
+        origin_model = [
+            [('orig_t1_w1', 0.8), ('orig_t1_w2', 0.1)],
+            [('orig_t2_w1', 1.0)]
+        ]
+        target_model = [
+            [('orig_t2_w1', 1.0)],
+            [('orig_t1_w2', 0.1), ('orig_t1_w1', 0.8)]
+        ]
+        converter = TopicModelConverter(origin_model, target_model)
+        target_vector = converter.compute_target_vector([0.25, 0.75])
+        self.assertEquals([0.75, 0.25], target_vector)
+
+    def test_compute_target_vector_with_target_encompass_origin_return_same_vector(self):
+        origin_model = [
+            [('orig_t1_w1', 0.8), ('orig_t1_w2', 0.1)],
+            [('orig_t2_w1', 1.0)]
+        ]
+        target_model = [
+            [('orig_t2_w1', 1.0)],
+            [('orig_t1_w2', 0.1), ('orig_t1_w1', 0.8)],
+            [('target_t3_w1', 0.4), ('target_t3_w2', 0.5)]
+        ]
+        converter = TopicModelConverter(origin_model, target_model)
+        target_vector = converter.compute_target_vector([0.25, 0.75])
+        self.assertEquals([0.75, 0.25, 0], target_vector)
+
+    def test_compute_target_vector_with_useless_dim_in_origin_return_same_vector(self):
+        origin_model = [
+            [('orig_t1_w1', 0.9), ('orig_t1_w2', 0.1)],
+            [('orig_t2_w1', 1.0)],
+            [('orig_t1_w1', 0.5), ('orig_t3_w2', 0.4)],
+        ]
+        target_model = [
+            [('orig_t2_w1', 1.0)],
+            [('orig_t1_w2', 0.1), ('orig_t1_w1', 0.9)]
+        ]
+        converter = TopicModelConverter(origin_model, target_model)
+        target_vector = converter.compute_target_vector([0.25, 0.75, 0])
+        self.assertEquals([0.75, 0.25], target_vector)
+
+    def test_compute_target_vector_with_two_topics_merged_return_merged_weights(self):
+        origin_model = [
+            [('orig_t1_w1', 0.9), ('orig_t1_w2', 0.1)],
+            [('orig_t2_w1', 1.0)]
+        ]
+        target_model = [
+            [('orig_t1_w1', 0.5), ('orig_t2_w1', 0.5)],
+            [('orig_t1_w2', 1.0)]
+        ]
+        converter = TopicModelConverter(origin_model, target_model)
+        target_vector = converter.compute_target_vector([0.5, 0.5])
+        self.assertEquals([0.95, 0.05], target_vector)
+
+    @staticmethod
+    def test_compute_target_vector_with_topic_split_return_split_weights():
+        origin_model = [
+            [('orig_t1_w1', 0.5), ('orig_t2_w1', 0.5)],
+            [('orig_t1_w2', 1.0)]
+        ]
+        target_model = [
+            [('orig_t1_w1', 0.9), ('orig_t1_w2', 0.1)],
+            [('orig_t2_w1', 1.0)]
+        ]
+        converter = TopicModelConverter(origin_model, target_model)
+        target_vector = converter.compute_target_vector([1.0, 0.0])
+        np.testing.assert_almost_equal([0.55, 0.5], target_vector, 3)
+
+    def test_compute_target_vector_with_no_shared_word_from_used_topic_return_zero(self):
+        origin_model = [
+            [('orig_t1_w1', 0.5), ('orig_t2_w1', 0.5)],
+            [('orig_t1_w2', 1.0)]
+        ]
+        target_model = [
+            [('target_t1_w1', 0.9), ('target_t1_w2', 0.1)],
+            [('orig_t1_w2', 1.0)]
+        ]
+        converter = TopicModelConverter(origin_model, target_model)
+        target_vector = converter.compute_target_vector([1.0, 0.0])
+        self.assertEquals([0, 0], target_vector)
+
+    def test_compute_target_vector_with_no_shared_word_between_models_return_zero(self):
+        origin_model = [
+            [('orig_t1_w1', 0.5), ('orig_t2_w1', 0.5)],
+            [('orig_t1_w2', 1.0)]
+        ]
+        target_model = [
+            [('target_t1_w1', 0.9), ('target_t1_w2', 0.1)],
+            [('target_t2_w1', 1.0)]
+        ]
+        converter = TopicModelConverter(origin_model, target_model)
+        target_vector = converter.compute_target_vector([0.5, 0.5])
+        self.assertEquals([0, 0], target_vector)
+
+
+if __name__ == '__main__':
+    unittest.main()
