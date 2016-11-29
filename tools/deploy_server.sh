eb3b37bb0ce6ca0a4d66ddb66e4beae7d3a361a9
nicoxx <nicoxx@hotmail.fr>
2016-11-29 08:17:36 +0100

    revert copy of local deps after server deployement

--- tools/deploy_server.sh
+++ tools/deploy_server.sh
@@ -4,3 +4,5 @@
 { cat src/server/local_deps.txt; echo; } | while read line; do cp -R "src/$line/$line" src/server; done
 # NB: 'preview should not be mandatory, I guess it's we use a pre-installed not up-to-date gcloud command line on travis, we should clean that
 gcloud preview app deploy src/server/app.yaml --stop-previous-version --quiet
+
+{ cat src/server/local_deps.txt; echo; } | while read line; do rm -r "src/server/$line"; done
