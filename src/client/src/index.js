15a4f2254e9c0418855eefc2cee707cc87f1e046
Nicolas Lecrique <nicoxx@hotmail.fr>
2017-01-04 00:04:26 +0100

    fix node cache

--- src/client/src/index.js
+++ src/client/src/index.js
@@ -3,12 +3,4 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-
 ReactDOM.render(<App/>, document.getElementById('root'));
-
-//
-//fetch(`/api/user/HELLO`).then(function(response) {
-//    return response.json().then(function(json) {
-//      ReactDOM.render(<App email={json.email} />, document.getElementById('root')
-//      )});
-//});
