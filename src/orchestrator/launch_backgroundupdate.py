fcad1642e1e9885f8acb17e4318f8a37c6da5b5e
Nicolas Lecrique <nicoxx@hotmail.fr>
2017-01-04 01:18:46 +0100

    Register api/react blueprints in main + rework env variables used in dev
    TEST_ENV has been splitted in two orthogonal notions:
    -TEST_ENV: I'm I running tests (true in travis, true if running tests in local)
    -DEV_ENV: I'm I in development mode (false in travis, true in local except for ISO travis/prod validation)
    
    DEV_ENV=True: use source code js served directly by node.js and server run directly with main => auto reload of sources, fast tests
    DEV_ENV=False: use optimized/compiled app served by server, use docker image for background_update: closest to prod, slower tests
    
    log manage those condition internally as it's common between the 2 clients (server, background)

--- src/orchestrator/launch_backgroundupdate.py
+++ src/orchestrator/launch_backgroundupdate.py
@@ -3,7 +3,7 @@
 
 from common.log import init_gcloud_log
 from orchestrator.backgroundupdate import update_model_profiles_userdocs
-from server.environment import IS_TEST_ENV, GCLOUD_PROJECT
+from server.environment import GCLOUD_PROJECT
 
-init_gcloud_log(GCLOUD_PROJECT, u'background_update', IS_TEST_ENV)
+init_gcloud_log(GCLOUD_PROJECT, u'background_update')
 update_model_profiles_userdocs()
